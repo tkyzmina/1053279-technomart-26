d871609d4fb74a07faa8a8d7f6c44ca1b95be211
tkyzmina <tkyzmina@gmail.com>
2019-10-25 13:51:37 +0300

    правки итог 25 окт

--- js/modal.js
+++ js/modal.js
@@ -1,60 +1,3 @@
-// var link = document.querySelector(".footer-contacts-button");
-// var popup = document.querySelector(".modal");
-// var close = popup.querySelector(".modal-close");
-// var modalOverlay = document.querySelector("#modal-overlay");
-// var form = popup.querySelector("form");
-// var login = popup.querySelector("[name=login]");
-// var password = popup.querySelector("[name=email]");
-// var comment = popup.querySelector("[name=comment]");
-// var isStorageSupport = true;
-// var storage = "";
-// try {
-//   storage = localStorage.getItem("name");
-// } catch (err) {
-//   isStorageSupport = false;
-// }
-// link.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   popup.classList.add("modal-show");
-//   modalOverlay.classList.add("overlay-show");
-//   if (storage) {
-//     login.value = storage;
-//     password.focus();
-//   } else {
-//     login.focus();
-//   }
-// });
-// modalOverlay.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   modalOverlay.classList.remove("overlay-show");
-//   popup.classList.remove("modal-show");
-// });
-// close.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   popup.classList.remove("modal-show");
-//   popup.classList.remove("modal-error");
-// });
-// form.addEventListener("submit", function(evt) {
-//   if (!login.value || !password.value || !comment.value) {
-//     evt.preventDefault();
-//     popup.classList.remove("modal-error");
-//     popup.offsetWidth = popup.offsetWidth;
-//     popup.classList.add("modal-error");
-//   } else {
-//     if (isStorageSupport) {
-//       localStorage.setItem("login", login.value);
-//     }
-//   }
-// });
-// window.addEventListener("keydown", function(evt) {
-//   if (evt.keyCode === 27) {
-//     evt.preventDefault();
-//     if (popup.classList.contains("modal-show")) {
-//       popup.classList.remove("modal-show");
-//       popup.classList.remove("modal-error");
-//     }
-//   }
-// });
 var link = document.querySelector(".footer-contacts-button");
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal-close");
@@ -77,3 +20,4 @@ close.addEventListener("click", function (evt) {
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
 });
+
